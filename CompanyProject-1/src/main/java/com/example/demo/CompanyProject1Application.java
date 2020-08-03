eafe054edae5a5a294651a4925bd1a83108a3d98
Veera Meena <Veera Meena@LAPTOP-U4JMTAAP>
2020-08-03 12:59:54 +0530

    "test project"

---
+++ CompanyProject-1/src/main/java/com/example/demo/CompanyProject1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyProject1Application.class, args);
+	}
+
+}
